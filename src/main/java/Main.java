e530e6dbe2b0e2f621815873b30827fb1b319d0c
jukkty <dima.iachimov@gmail.com>
2021-03-23 09:03:11 +0200

    Main updated but old version is also here

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,3 +7,16 @@ public class Main {
     System.out.println(bonus);
   }
 }
+//public class Main {
+//  public static void main(String[] args) {
+//    BonusService service = new BonusService();
+//
+//    long amount = 1000_60;
+//    boolean registered = true;
+//    long expected = 30;
+//
+//    long actual = service.calculate(amount, registered);
+//    boolean passed = expected == actual;
+//    System.out.println(passed);
+//  }
+//}
